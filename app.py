947fb75be3be84b6db60c752297eca180701aada
matome <matomesediana@gmail.com>
2026-05-22 10:41:12 +0200

    fix render port

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def home():
     return render_template("index.html", messages=messages)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
